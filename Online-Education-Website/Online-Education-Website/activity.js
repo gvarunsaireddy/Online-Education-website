35de1a2e911abf33a2f632a0b7510298b1164a15
gvarunsaireddy <148617814+gvarunsaireddy@users.noreply.github.com>
2026-02-09 21:27:52 +0530

    Add files via upload

---
+++ Online-Education-Website/Online-Education-Website/activity.js
@@ -0,0 +1,80 @@
+
+const navbar = document.querySelector('.NavigationBar');
+const menu = document.querySelector('#MenuBtn');
+
+menu.onclick = () =>{ 
+
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('AndroidBoy_active');
+}
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+  var swiper = new Swiper(".ourteacher", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".StudentsReviews", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+  document.getElementById('year').textContent = new Date().getFullYear();
